cfdf8d49ade916d9c93a690b0346768a6451d8f2
premit96 <45023895+premit96@users.noreply.github.com>
2023-04-12 12:31:50 -0400

    Add files via upload

---
+++ Finance-Dashboard-main/README.md
@@ -0,0 +1,34 @@
+# Finance-Dashboard
+Finance dashboard fullstack (MERN) app with redux-toolkit state management, written in React (Vite) and TypeScript. Frontend written in Vite, Redux-Toolkit, MUI (styles and components) and Recharts library (charts). Backend in Node.js, Express.js and MongoDB for database.
+
+
+## 📦 Tech stack:
+### Frontend
+- React.js / Vite
+- TypeScript
+- Redux-toolkit
+- MUI
+- Recharts
+
+### Backend
+- Node.js
+- Express.js
+- MongoDB
+
+
+## 💻 Demo
+Click the link and check the app: [Link](https://ms-finance-dashboard.vercel.app/)
+
+### Screenshot
+
+![](./client/public/assets/dashboard.JPG)
+![](./client/public/assets/predicstions.JPG)
+ 
+
+## Environment variables:
+After creating your own MongoDB database, add .env file to `server` directory and to add following data of your own:
+MONGODB_URL="mongodb+srv://<your-username>:<your-password>.7km6ofs.mongodb.net/<your-database-name>?retryWrites=true&w=majorityk"
+PORT="port number, e.g 8080"
+
+
+Sample data in /client/data/data.js for data structure reference.
---
+++ Finance-Dashboard-main/client/src/App.tsx
@@ -0,0 +1,39 @@
+import { Box, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./pages/navbar";
+import Dashboard from "./pages/dashboard";
+import Predictions from "./pages/predictions";
+
+function App() {
+    const theme = useMemo(() => createTheme(themeSettings), []);
+
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box
+                        width="100%"
+                        height="100%"
+                        padding="1rem 2rem 4rem 2rem"
+                    >
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route
+                                path="/predictions"
+                                element={<Predictions />}
+                            />
+                        </Routes>
+                    </Box>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
